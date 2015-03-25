237d057559bcacb6604f239c98f8ccd25a4480ca
Clint <clintp1@gmail.com>
2015-03-25 10:45:53 +0000

    fizzbuzz in C# test driven using NUnit

---
+++ GameClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace FizzBuzz2
+{
+	public class GameClass
+	{
+		private string shout;
+
+		public static void Main(string[] arg)
+		{
+			GameClass obj = new GameClass ();
+			Console.WriteLine ("Enter a Number: ");
+			string numberString = Console.ReadLine ();
+			int number = int.Parse (numberString);
+			obj.DivisibleBy3 (number);
+			obj.DivisibleBy5 (number);
+			obj.DivisibleBy15 (number);
+			Console.WriteLine (obj.Shout);
+		}
+
+		public void DivisibleBy15(int number)
+		{
+			if (number  % 15 == 0) {
+				shout = "FizzBuzz";
+			} 
+		}
+
+		public void DivisibleBy3(int number)
+		{
+			if (number  % 3 == 0) {
+				shout = "Fizz";
+			} 
+		}
+
+		public void DivisibleBy5(int number)
+		{
+			if (number  % 5 == 0) {
+				shout = "Buzz";
+			} 
+		}
+
+		public string Shout
+		{
+			get { return shout; }
+		}
+	}
+}
---
+++ GameTest.cs
@@ -0,0 +1,31 @@
+﻿namespace FizzBuzz2
+{
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class GameClassTest
+	{
+		[Test]
+		public void DivisibleBy15()
+		{
+			GameClass numberFizzBuzz = new GameClass();
+			numberFizzBuzz.DivisibleBy15 (15);
+			Assert.AreEqual("FizzBuzz", numberFizzBuzz.Shout);
+		}
+		[Test]
+		public void DivisibleBy3()
+		{
+			GameClass numberFizzBuzz = new GameClass();
+			numberFizzBuzz.DivisibleBy3 (3);
+			Assert.AreEqual("Fizz", numberFizzBuzz.Shout);
+		}
+
+		[Test]
+		public void DivisibleBy5()
+		{
+			GameClass numberFizzBuzz = new GameClass();
+			numberFizzBuzz.DivisibleBy5 (5);
+			Assert.AreEqual("Buzz", numberFizzBuzz.Shout);
+		}
+	}
+}
